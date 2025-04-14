df9ea94856b6003a8bf2a8958304867abd0dd873
M. Ahsan Shoaib <113696186+mahsanshoaib@users.noreply.github.com>
2025-04-14 09:41:17 +0500

    Update text_emotion.py

--- text_emotion.py
+++ text_emotion.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 class TextEmotionDetector:
-    def __init__(self, model_path="models/text_model"):
+    def __init__(self, model_path="text_model"):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         self.model = AutoModelForSequenceClassification.from_pretrained(model_path)
